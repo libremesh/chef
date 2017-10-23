87498c27127c6174632247e03c9f252b26f02c65
Paul Spooren <paul@spooren.de>
2017-10-24 00:58:54 +0200

    rename jefe to online imagebuilder and change server

--- static/imagebuilder.js
+++ static/imagebuilder.js
@@ -1,4 +1,5 @@
-server_address = "https://betaupdate.libremesh.org/";
+data = {};
+data.update_server = "https://ledeupdate.planetexpress.cc"
 edit_packages_bool = false;
 packages_flavor = [];
 
@@ -15,7 +16,7 @@ function search() {
 	var distro = document.getElementById("distro").value;
 	var release = document.getElementById("release").value;
 
-	request_url = server_address + "api/models?model_search=" + device + "&distro=" + distro + "&release=" + release
+	request_url = data.update_server + "/api/models?model_search=" + device + "&distro=" + distro + "&release=" + release
 
 	var xmlhttp = new XMLHttpRequest();
 	xmlhttp.open("GET", request_url, true);
@@ -47,7 +48,7 @@ function search() {
 
 function load_distros() {
 	var xmlhttp = new XMLHttpRequest();
-	request_url = server_address + "api/distros";
+	request_url = data.update_server + "/api/distros";
 
 	xmlhttp.open("GET", request_url, true);
 
@@ -97,7 +98,7 @@ function profile_changed() {
 
 function load_network_profiles() {
 	var xmlhttp = new XMLHttpRequest();
-	request_url = server_address + "api/network_profiles";
+	request_url = data.update_server + "/api/network_profiles";
 
 	xmlhttp.open("GET", request_url, true);
 
@@ -124,7 +125,7 @@ function load_releases() {
 	var distro = document.getElementById("distro").value;
 	var release = document.getElementById("release").value;
 
-	request_url = server_address + "api/releases"
+	request_url = data.update_server + "/api/releases"
 
 	xmlhttp.open("GET", request_url, true);
 
@@ -157,7 +158,7 @@ function load_packages_image() {
 	set_device_info()
 	if(typeof target != 'undefined' && typeof subtarget != 'undefined' && typeof profile != 'undefined') {
 
-		request_url = server_address + "api/packages_image?distro=" + distro + "&release=" + release + "&target=" + target + "&subtarget=" + subtarget+ "&profile=" + profile
+		request_url = data.update_server + "/api/packages_image?distro=" + distro + "&release=" + release + "&target=" + target + "&subtarget=" + subtarget+ "&profile=" + profile
 
 		xmlhttp.open("GET", request_url, true);
 
@@ -283,4 +284,117 @@ function bootstrap() {
 	load_network_profiles();
 	load_flavors();
 	toggle_advanced_view();
+}
+
+
+// shows notification if update is available
+function info_box(info_output) {
+	document.getElementById("info_box").style.display = "block";
+	document.getElementById("info_box").innerHTML = info_output;
+}
+
+function error_box(error_output) {
+	document.getElementById("error_box").style.display = "block";
+	document.getElementById("error_box").innerHTML = error_output;
+	document.getElementById("info_box").style.display = "none";
+}
+
+// requests to the update server
+function server_request(request_dict, path, callback) {
+	url = data.update_server + "/" + path
+	var xmlhttp = new XMLHttpRequest();
+	xmlhttp.open("POST", url, true);
+	xmlhttp.setRequestHeader("Content-type", "application/json");
+	xmlhttp.send(JSON.stringify(request_dict));
+	xmlhttp.onerror = function(e) {
+		console.log("update server down")
+	}
+	xmlhttp.addEventListener('load', function(event) {
+		callback(xmlhttp)
+	});
+}
+
+function image_request() {
+	request_dict = {}
+	request_dict.distro = document.request_form.distro.value;
+	request_dict.version = document.request_form.release.value;
+	profile_split = document.request_form.profile.value.split("/");
+	request_dict.target = profile_split[0]
+	request_dict.subtarget = profile_split[1]
+	request_dict.board = profile_split[2]
+	request_dict.network_profile = document.request_form.network_profile.value
+	if (packages != "") {
+		request_dict.packages = packages
+	}
+	server_request(request_dict, "image-request", image_request_handler)
+}
+
+function image_request_handler(response) {
+	if (response.status === 400) {
+		response_content = JSON.parse(response.responseText)
+		error_box_content = response_content.error
+		if('log' in response_content) {
+			error_box_content += ' <a href="' + response_content.log + '">Build log</a>'
+		}
+		error_box(error_box_content)
+
+	} else if (response.status === 500) {
+		error_box("internal server error. please try again later")
+	} else if (response.status === 503) {
+		error_box("please wait. server overloaded")
+		// handle overload
+		setTimeout(image_request, 30000)
+	} else if (response.status === 201) {
+		response_content = JSON.parse(response.responseText)
+		if(response_content.queue != undefined) {
+			// in queue
+			info_box("please wait. you are in queue position " + response_content.queue)
+			console.log("queued")
+		} else {
+			info_box("imagebuilder not ready, please wait")
+			console.log("setting up imagebuilder")
+		}
+		setTimeout(image_request, 5000)
+	} else if (response.status === 206) {
+		// building
+		console.log("building")
+		info_box("building image")
+		setTimeout(image_request, 5000)
+	} else if (response.status === 200) {
+		// ready to download
+		response_content = JSON.parse(response.responseText)
+		files_url = response_content.files
+		load_files();
+		info_box("image created. Download: <a href='" + response_content.sysupgrade + "'>Firmware</a>, <a href='" + response_content.sysupgrade + ".log'>Build log</a>, <a href='" + response_content.sysupgrade + ".sig'>Signature</a>")
+	}
+}
+
+function load_files() {
+	var xmlhttp = new XMLHttpRequest();
+
+	xmlhttp.open("GET", files_url, true);
+
+	xmlhttp.onreadystatechange = function () {
+		if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+			releases_results(xmlhttp);
+		}
+	}
+	xmlhttp.send(null);
+
+	function releases_results(xmlhttp) {
+		response_content = JSON.parse(xmlhttp.responseText);
+		files_box = document.getElementById("files_box")
+		files_box.innerHTML = ""
+		var list = document.createElement('ul');
+
+		for(var i = 0; i < response_content.length; i++) {
+			var item = document.createElement('li');
+			var link = document.createElement('a');
+			link.href = files_url + response_content[i].name
+			link.innerHTML = response_content[i].name
+			item.appendChild(link)
+			list.appendChild(item);
+		}
+		files_box.appendChild(list);
+	}
 }
