fe076a59db2378cc26631d91fcb697fcf52bc9b0
Paul Spooren <paul@spooren.de>
2017-11-29 12:00:08 +0100

    fix #23

--- tables.sql
+++
@@ -1,9 +0,0 @@
-create table if not exists devices (
-	brand text not null,
-	model text not null,
-	versions text,
-	current text no null,
-	target text not null,
-	subtarget text not null,
-	primary key(brand, model, versions)
-);
